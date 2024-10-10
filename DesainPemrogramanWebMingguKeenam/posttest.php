ec8b53d0e7aa05e4eec519e62b8e6aad12f33377
Naditya206 <aditandino2019211@gmail.com>
2024-10-10 08:40:18 +0700

    update

---
+++ DesainPemrogramanWebMingguKeenam/posttest.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+            font-family: Arial, sans-serif;
+        }
+        table, th, td {
+            border: 1px solid #000;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $siswa = [
+        [
+            'nama' => 'Andi',
+            'umur' => '15',
+            'kelas' => '10A',
+            'alamat' => 'Malang',
+        ],
+        [
+            'nama' => 'Siti',
+            'umur' => '16',
+            'kelas' => '10B',
+            'alamat' => 'Batu',
+        ],
+        [
+            'nama' => 'Budi',
+            'umur' => '15',
+            'kelas' => '10A',
+            'alamat' => 'Dinoyo',
+        ],
+        [
+            'nama' => 'Anton',
+            'umur' => '25',
+            'kelas' => '15A',
+            'alamat' => 'Dinoyo',
+        ],
+    ];
+
+    $totalUmur = 0;
+    $jumlahSiswa = count($siswa);
+
+    foreach ($siswa as $data) {
+        $totalUmur += $data['umur'];
+    }
+    $rataUmur = $totalUmur / $jumlahSiswa;
+    ?>
+
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Umur</th>
+            <th>Kelas</th>
+            <th>Alamat</th>
+        </tr>
+        <?php foreach ($siswa as $data) : ?>
+        <tr>
+            <td><?php echo $data['nama']; ?></td>
+            <td><?php echo $data['umur']; ?></td>
+            <td><?php echo $data['kelas']; ?></td>
+            <td><?php echo $data['alamat']; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+
+    <h2 style="text-align: center;">Rata-rata Umur Siswa: <?php echo number_format($rataUmur, 2)?></h2>
+</body>
+</html>
